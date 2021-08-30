1e27cacb1fa0141ac60bf427c25b95c78531f48f
anggaprasetya-fs <officialfanx52@gmail.com>
2021-08-30 07:58:32 +0700

    First Update

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  changeBookDataHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: changeBookDataHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+
+];
+
+module.exports = routes;
